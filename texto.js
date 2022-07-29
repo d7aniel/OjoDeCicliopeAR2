eb8311b817e95a1f7f672401803ff3a3c37923b0
D7aniel <cohl.daniel@gmail.com>
2022-07-29 20:09:57 -0300

    Reposicionar bien

--- texto.js
+++ texto.js
@@ -18,7 +18,7 @@ class Texto {
     this.titulo.style.textAlign = "center";
     this.subtitulo.style.textAlign = "center";
 
-    this.titulo.innerText = "El ojo del ciclope v10 Lobos";
+    this.titulo.innerText = "El ojo del ciclope v11 Lobos";
     this.subtitulo.innerText = "version flores 1";
 
     this.texto.append(this.titulo);
