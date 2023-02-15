09d64a5746058eec2b8819d31a68c175c765d7a2
D7aniel <cohl.daniel@gmail.com>
2023-02-15 22:40:30 +0100

    actulizacion

--- index.js
+++ index.js
@@ -275,13 +275,15 @@ for (let i = 0; i < lista.length; i++) {
 }
 // console.log(t);
 // ---- POSICIONES REALES
+console.log("act. 1");
 let listaDePosiciones = [
   { lat: -37.8932134, lon: -58.27464, rot: 0 + Math.PI * 0.7, alto: 10 },
-  { lat: -37.8965, lon: -58.2776, rot: 2.237 - Math.PI * 0.3, alto: 10 }, //trigal
+  { lat: -37.89659, lon: -58.27779, rot: 3.1415 + Math.PI * 0.9, alto: 10 }, //trigal
   { lat: -37.894, lon: -58.2751768, rot: 5.982 + Math.PI * 0.5, alto: 10 },
   { lat: -37.8957727, lon: -58.2767, rot: 2.237 + Math.PI * 0.5, alto: 10 },
-  { lat: -37.89659, lon: -58.27779, rot: 3.1415 + Math.PI * 0.9, alto: 10 }, //sierra chata desde descampado
+  { lat: -37.8968, lon: -58.27825, rot: 3.1415 + Math.PI * 0.9, alto: 10 }, //sierra chata desde descampado
   { lat: -37.8949, lon: -58.27599, rot: 2.237, alto: 10 },
+  // [-58.2779599, -37.8966535], //sierra chata desde descampado
 
   { lat: -37.89289, lon: -58.27358, rot: Math.PI, alto: 11 },
   { lon: -58.27365953785302, lat: -37.89359786626062, rot: -2.237 + Math.PI * 0.5, alto: 20 },
