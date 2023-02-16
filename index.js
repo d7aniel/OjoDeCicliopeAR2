107c4ff8a6033eb8e13cc5fcfcf1e0c0cba2681a
D7aniel <cohl.daniel@gmail.com>
2023-02-16 21:10:57 +0100

    cambio de ubicacion

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ let listaCuadros = [
 
 let lista = [
   [-58.27464, -37.8932134],
-  [-58.2781, -37.89656], //trigal
+  [-58.2776, -37.8965], //trigal
   [-58.2751768, -37.894],
   [-58.2767, -37.8957727],
   [-58.2779599, -37.8966535], //sierra chata desde descampado
@@ -376,13 +376,13 @@ for (let i = 0; i < lista.length; i++) {
 // ---- POSICIONES REALES
 console.log("act. 6");
 //suma a la izq
-//resta a la der
+//resta a la der  2781
 let listaDePosiciones = [
   { lat: -37.8932134, lon: -58.27464, rot: 0 + Math.PI * 0.7, alto: 10 },
-  { lat: -37.89656, lon: -58.2781, rot: 2.237 - Math.PI * 0.4, alto: 10 }, //trigal
+  { lat: -37.89659, lon: -58.27779, rot: 2.237 - Math.PI * 0.4, alto: 10 }, //trigal
   { lat: -37.894, lon: -58.2754, rot: 5.982 + Math.PI * 0.5, alto: 10 },
   { lat: -37.8957727, lon: -58.2766, rot: 2.237 + Math.PI * 0.5, alto: 10 },
-  { lat: -37.8968, lon: -58.27801, rot: 2.237 - Math.PI * 0.5, alto: 10 }, //el del final sierra chata desde descampado
+  { lat: -37.89656, lon: -58.2781, rot: 2.237 - Math.PI * 0.5, alto: 10 }, //el del final sierra chata desde descampado
   { lat: -37.8949, lon: -58.27599, rot: 2.237, alto: 10 },
   // [-58.2779599, -37.8966535], //sierra chata desde descampado
 
