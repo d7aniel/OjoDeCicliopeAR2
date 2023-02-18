917ae8017b568aec6a3087a350b4758135c28e3b
D7aniel <cohl.daniel@gmail.com>
2023-02-18 16:50:49 +0100

    cambios

--- texto.js
+++ texto.js
@@ -18,7 +18,7 @@ class Texto {
     this.titulo.style.textAlign = "center";
     this.subtitulo.style.textAlign = "center";
 
-    this.titulo.innerText = "Cargando...";
+    this.titulo.innerText = "Abriendo...";
     this.subtitulo.innerText = "Espere unos segundos por favor";
 
     this.texto.append(this.titulo);
