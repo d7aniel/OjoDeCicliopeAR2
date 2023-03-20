a55043cfbfa85cff99766a8b726a0dcded517f8e
D7aniel <cohl.daniel@gmail.com>
2023-03-20 15:30:09 +0100

    posicion

--- index.js
+++ index.js
@@ -476,11 +476,11 @@ console.log("act. 10");
 //resta a la der  2781
 // -57.968722, -34.903066;
 let listaDePosiciones = [
-  { lat: -34.903066, lon: -57.968722, rot: 0 + Math.PI * 0.7, alto: 10 },
-  // { lat: -37.8932134, lon: -58.27464, rot: 0 + Math.PI * 0.7, alto: 10 },//real
+  // { lat: -34.903066, lon: -57.968722, rot: 0 + Math.PI * 0.7, alto: 10 },
+  { lat: -37.8932134, lon: -58.27464, rot: 0 + Math.PI * 0.7, alto: 10 }, //real
   // -34.90356440168241, -57.96781789655587
-  { lat: -34.859983, lon: -58.078134, rot: 2.237 - Math.PI * 0.7, alto: 10 }, //trigal
-  // { lat: -37.8964, lon: -58.27765, rot: 2.237 - Math.PI * 0.7, alto: 10 }, //trigal real
+  // { lat: -34.859983, lon: -58.078134, rot: 2.237 - Math.PI * 0.7, alto: 10 }, //trigal
+  { lat: -37.8964, lon: -58.27765, rot: 2.237 - Math.PI * 0.7, alto: 10 }, //trigal real
   { lat: -37.894, lon: -58.2754, rot: 5.982 + Math.PI * 0.5, alto: 10 },
   ///////////////////
   { lat: -37.8957727, lon: -58.2766, rot: 2.237 + Math.PI * 0.5, alto: 10 },
